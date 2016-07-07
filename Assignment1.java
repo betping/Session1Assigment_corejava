6be5510a62c967a59d1d5b729f54217855eeff22
betping <betping1993@gmail.com>
2016-07-07 17:58:57 +0530

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -4,14 +4,15 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
    Scanner input =new Scanner(System.in);
-		int num1 =0 ,   num=0;
-		int Sum;
+		int num1 =0 ;
+		float  num=0;
+		float Sum;
 		System.out.println("Enter frist num1:");
 	         num1=input.nextInt();
 		System.out.println("Enter Second num:");
 		 num=input.nextInt();
 		Sum=num1+num;
-		System.out.println("Sumof two num"+Sum);
+		System.out.println("Sum of two num"+Sum);
 		
 		
 		
